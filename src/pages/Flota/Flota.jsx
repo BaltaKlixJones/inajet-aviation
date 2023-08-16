38e859148af8fdd2bc1834731968d8e6a2c99c3e
Baltasar Klix Jones <balta98_@hotmail.com>
2023-08-16 10:52:46 -0300

    aclaraciones

--- src/pages/Flota/Flota.jsx
+++ src/pages/Flota/Flota.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./Flota.css";
 import { useDispatch } from "react-redux";
 import Cards from "../../components/Cards/Cards";
@@ -8,39 +8,35 @@ import { useTranslation } from "react-i18next";
 
 const Flota = () => {
   const [t, i18n] = useTranslation("global");
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getAllAirCrafts())
-    // setTimeout(() => {
-    //   setLoading(false)
-    // }, 1000)
-    .then(() => {
-      setLoading(false)
-    })
-  },[])
+    dispatch(getAllAirCrafts()).then(() => {
+      setLoading(false);
+    });
+  }, []);
 
-  if(loading) return <Loading/>
+  if (loading) return <Loading />;
 
   return (
     <>
-    <div className="container-flota">
-      <div className="container-titulo">
-        <h1 className="titulo-flota">
-          <span style={{ fontWeight: "bold", marginRight: "15px" }}>{t("text.live")}</span>
-          {t("text.experience")}{" "} INAJET
-        </h1>
-       
+      <div className="container-flota">
+        <div className="container-titulo">
+          <h1 className="titulo-flota">
+            <span style={{ fontWeight: "bold", marginRight: "15px" }}>
+              {t("text.live")}
+            </span>
+            {t("text.experience")} INAJET
+          </h1>
+        </div>
+        <div className="gradient-overlay"></div>
       </div>
-      <div className="gradient-overlay"></div>
-    </div>
-    {/* <h1 className="titulo-flota" style={{display:"flex", alignItems:"center", justifyContent: "center"}}>FLOTA</h1> */}
-      
-
       <Cards />
-     
-</>
+      <div className="container_aclaracion">
+        <h5>{t("text.aclaracion")}</h5>
+      </div>
+    </>
   );
 };
 
