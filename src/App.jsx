5d1295051ba1269638c449355c529e74bd1ca82f
Baltasar Klix Jones <balta98_@hotmail.com>
2023-08-18 14:42:06 -0300

    ananalytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,30 @@ import Flota from "./pages/Flota/Flota";
 import Services from "./pages/Services/Services";
 import Footer from "./pages/Footer/Footer";
 import Perfil from "./pages/Perfil/Perfil";
-import Loading from "./pages/Loading/Loading";
-import axios from "axios";
-import Contacto from "./pages/Contacto/Contacto";
 import Terms from "./pages/Terms/Terms";
+import Contacto from "./pages/Contacto/Contacto";
 
+import axios from "axios";
 axios.defaults.baseURL = `${import.meta.env.VITE_BASE_URL}`;
+
+import { inject } from "@vercel/analytics";
+
 function App() {
+  inject();
   return (
     <>
       <NavBar />
       <ScrollToTop />
       <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route exact path="/flota" element={<Flota/>} />
-        <Route exact path="/services" element={<Services/>} />
-        <Route exact path="/aircraft/:id" element={<Perfil/>} />
-        <Route exact path="/loading" element={<Loading/>} />
-        <Route exact path="/consultar/:id" element={<Home/>} />
-        <Route exact path="/contacto" element={<Contacto/>} />
-        <Route exact path="/terminosyCondiciones" element={<Terms/>} />
-        <Route exact path="/loading" element={<Loading/>} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/flota" element={<Flota />} />
+        <Route exact path="/services" element={<Services />} />
+        <Route exact path="/aircraft/:id" element={<Perfil />} />
+        <Route exact path="/consultar/:id" element={<Home />} />
+        <Route exact path="/contacto" element={<Contacto />} />
+        <Route exact path="/terminosyCondiciones" element={<Terms />} />
       </Routes>
-    
+
       <Footer />
     </>
   );
