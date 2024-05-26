05b547fcb0cc1ec106496aaad7e97888421728d6
Baltasar Klix Jones <109623044+BaltaKlixJones@users.noreply.github.com>
2024-05-26 12:06:18 -0300

    order aircrafts

--- src/utils/aircraftsOrder.js
+++ src/utils/aircraftsOrder.js
@@ -1,11 +1,28 @@
 export const aircraftOrder = [
   "Piper PA-46R-350T Matrix ",
+  "Cessna Caravan",
   "Tecnam P2012 Traveller ",
   "Beechcraft King Air 200GT ",
+  "Metroliner III",
+  "Cessna Citation Mustang",
   "Cessna 525 Citation Jet ",
   "Bombardier Learjet 60 ",
   "Hawker 800XP ",
+  "Embraer Legacy 600",
   "Gulfstream IV SP ",
   "Gulfstream V",
   "Embraer ERJ145 ",
 ];
+// Piper PA-46R-350T Matrix
+// Cessna Caravan (NUEVO)
+// Tecnam P2012 Traveller
+// Beechcraft King Air 200GT
+// Metroliner III (NUEVO)
+// Cessna Citation Mustang (NUEVO)
+// Cessna 525 Citation Jet
+// Bombardier Learjet 60
+// Hawker 800XP
+// Embraer ERJ145
+// Embraer Legacy 600 (NUEVO)
+// Gulfstream IV SP
+// Gulfstream V
