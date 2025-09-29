a4452486302b87b25cd549026285bb0e39a3746c
Baltasar Klix Jones <balta98_@hotmail.com>
2025-09-28 23:02:02 -0300

    change wpp number2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
-import Flota from "./pages/Flota/Flota";
+// import Flota from "./pages/Flota/Flota";
 import Services from "./pages/Services/Services";
 import Footer from "./pages/Footer/Footer";
 import Perfil from "./pages/Perfil/Perfil";
@@ -25,8 +25,7 @@ function App() {
       <ScrollToTop />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        {/* <Route exact path="/flota" element={<Flota />}.
-         /> */}
+        {/* <Route exact path="/flota" element={<Flota />}. /> */}
         <Route exact path="/services" element={<Services />} />
         <Route exact path="/aircraft/:id" element={<Perfil />} />
         <Route exact path="/consult/:id" element={<Home />} />
